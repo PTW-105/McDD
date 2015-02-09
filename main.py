3fade85e9147b67f18739f9fb68c03d7c78fe49b
PTW-105 <jstemp105@gmail.com>
2015-02-08 19:22:27 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+###################################
+# Title: McAfee Debug & Diag Tool
+# Version: 0.1a
+# Author: Josh Stemp
+# Contact: jstemp105@gmail.com
+# Copyright Joshua Stemp 2014
+###################################
+
